9f3e8e40307384eedc01c1db7aa1d4b086476643
Max Xu <max.jn.xu04@gmail.com>
2025-03-29 22:42:47 -0400

    Error handling

--- Torch_Hot_Fire/pressure_transducer.py
+++ Torch_Hot_Fire/pressure_transducer.py
@@ -1,26 +1,31 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 from labjack import ljm
+from AdaptiveData import Buffer
 
 class PressureTransducer:
-    def __init__(self, name, input_channel, x, y, parent):
+    def __init__(self, name, input_channel_1, input_channel_2, x, y, parent):
         self.name = name
-        self.input_channel = input_channel
+        self.input_channel_1 = input_channel_1
+        self.input_channel_2 = input_channel_2
         self.label = QtWidgets.QLabel("0.00 psi", parent)
         self.label.setGeometry(x, y, 100, 25)
         self.label.setStyleSheet("background-color: #00baff; color: white; font-size: 12pt; font-weight: bold;")
         self.label.setAlignment(Qt.AlignCenter)
         self.pressure = 0.0
-        self.data = []
+        self.data = Buffer(100)
 
     def update_pressure(self, handle):
         try:
-            voltage = ljm.eReadName(handle, self.input_channel)
+            voltage_1 = ljm.eReadName(handle, self.input_channel_1)
+            voltage_2 = ljm.eReadName(handle, self.input_channel_2)
             excitation_voltage = 12.0
-            full_scale_output = (3.00 + 0.015) * 1e-3 * excitation_voltage
-            pressure_full_scale = 30000  # 30,000 PSI max
-            self.pressure = (voltage / full_scale_output) * pressure_full_scale
+            full_scale_output = 3.00 - 0.015 * 1e-3 * excitation_voltage
+            pressure_full_scale = 10000  # 10,000 PSI max
+            self.pressure = ((voltage_1 - voltage_2) / full_scale_output) * pressure_full_scale
             self.label.setText(f"{self.pressure:.2f} psi")
             self.data.append(self.pressure)
         except Exception as e:
-            print(f"Error reading pressure from {self.input_channel}: {e}")
+            self.pressure = float('nan')
+            self.data.append(self.pressure)
+            raise Exception(f"Error reading pressure from {self.input_channel_1}, {self.input_channel_2}: {e}")
