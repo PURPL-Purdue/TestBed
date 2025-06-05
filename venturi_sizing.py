26a5d490e54f4ac7104dec8e76b964b4e1f35c27
MineLaser <139770515+MineLaser@users.noreply.github.com>
2025-06-05 14:08:34 -0400

    Idk random changes

--- venturi_sizing.py
+++ venturi_sizing.py
@@ -14,55 +14,44 @@ from pyfluids import Fluid, FluidsList, Input
 #  CONSTANTS
 # ──────────────────────────────────────────────────────────────
 
-# rho = pyfluids.Fluid(FluidsList) # Water density at ambient pressure and temp
-C_d_vent = 0.905 # Discharge coefficient guess (based on PSP Liquids' testing)
-# https://purdue-space-program.atlassian.net/wiki/spaces/PL/pages/936903267/Successful+Water+Flow+1
+C_d = 0.905 # Discharge coefficient guess (based on PSP Liquids' testing) https://purdue-space-program.atlassian.net/wiki/spaces/PL/pages/936903267/Successful+Water+Flow+1
 p_1 = 650 # Pressure downstream of cavitating venturi (psi)
 x = 0.9 # Anticipated pressure recovery across the venturi
-A = np.pi * (0.426 / 2) ** 2# Cross sectional are of a -8 tube (in^2)
-
-p_0 = p_1 / x # Pressure upstream of the cavitating venturi (psi)
-
-# ──────────────────────────────────────────────────────────────
-#  UNIT CONVERSIONS
-# ──────────────────────────────────────────────────────────────
-
-p_0_pa = p_0 * 6894.76 # Pressure upstream of cavitating venturi (Pa)
 m_dot_kg = 0.961 # Desired mass flow (kg/s)
+rho = 810 # RP-1 density (from internet :) )
+p_0 = p_1 / x # Pressure upstream of the cavitating venturi (psi)
+p_0_pa = p_0 * 6894.76 # Pressure upstream of cavitating venturi (Pa)
+d = 0.426 # Line inner diameter (in)
+A = np.pi * (d / 2) ** 2 # Cross sectional are of a -8 tube (in^2)
 A_m2 = A / 1550 # Cross sectional are of a -8 tube (m^2)
 
 # ──────────────────────────────────────────────────────────────
 #  CALCULATIONS
 # ──────────────────────────────────────────────────────────────
 
-water = Fluid(FluidsList.Water).with_state(Input.pressure(p_0_pa), Input.temperature(20))
-rho = 810 # RP-1 density
-#water.density # Water density (kg/m^3)
-
+# rho = Fluid(FluidsList.Water).with_state(Input.pressure(p_0_pa), Input.temperature(20)).density
 p_sat_pa = Fluid(FluidsList.Water).with_state(Input.quality(0.0), Input.temperature(20)).pressure # Water saturation pressure at 20 degrees celsius
+# This should be changed because RP-1's saturation pressure is different
 
 # We assume cavitation to calculate the venturi throat area
-A_star_m2 = m_dot_kg / (C_d_vent * np.sqrt(2 * rho * (p_0_pa - p_sat_pa))) # Throat area (m^2)
+A_star_m2 = m_dot_kg / (C_d * np.sqrt(2 * rho * (p_0_pa - p_sat_pa))) # Throat area (m^2)
+
+# Modified bernoulli equation used to calculate throat area
+# A_star_m2 = 1 / np.sqrt( (1 / A_m2**2) - (2 * rho * (p_sat_pa - p_0_pa)) / m_dot_kg**2 )
+
 d_star_m = 2 * np.sqrt(A_star_m2 / np.pi) # Venturi throat diameter (m)
-
-# Now we have to plug the throat area into bernoulli to see if we actually reach cavitation
-A_ratio = A_m2 / A_star_m2
-V_dot = m_dot_kg / rho # Volumetric flow (m^3/s)
-v_line = V_dot / A_m2 # Line velocity (m/s)
-
-# Actual throat pressure (Pa)
-p_star_pa = p_0_pa + 0.5 * rho * v_line ** 2 * (1 - A_ratio ** 2)
-
-p_star = p_star_pa / 6894.76 # Actual throat pressure (psi)
+d_star_in = d_star_m * 39.3701 # Venturi throat diameter (in)
 
 # ──────────────────────────────────────────────────────────────
 #  RESULTS
 # ──────────────────────────────────────────────────────────────
 
-d_star_in = d_star_m * 39.3701 # Venturi throat diameter (in)
+print(f"\nAnticipated C_d:                 {C_d}")
+print(f"Desired mass flow:               {m_dot_kg:.3f} kg/s")
 
-print(f"Anticipated C_d: {C_d_vent}")
-print(f"Upstream venturi pressure: {p_0:.0f} psi\n")
-print(f"Downstream venturi pressure: {p_1:.0f} psi\n")
-print(f"Venturi throat diameter: {round(d_star_in,3)} in")
-print(f"Actual throat pressure: {p_star:.2f} psi")
+print(f"\nUpstream venturi pressure:       {p_0:.0f} psi")
+print(f"Expected throat pressure:        {(p_sat_pa / 6894.76):.1f} psi")
+print(f"Downstream venturi pressure:     {p_1:.0f} psi")
+
+print(f"Line inner diameter:             {0.426:.3f} in")  # assuming -8 tube ID
+print(f"Venturi throat diameter:         {(d_star_m * 39.3701):.3f} in\n")
