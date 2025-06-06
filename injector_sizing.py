8d99657a7d5e0444d9c94c2e31fb507ca0aaa6e5
MineLaser <139770515+MineLaser@users.noreply.github.com>
2025-06-06 14:14:10 -0400

    Added variable injector stiffness

--- injector_sizing.py
+++ injector_sizing.py
@@ -27,8 +27,9 @@ C_d_fu = 0.8 # Fuel orifice anticipated C_d (N/A)
 F = 500 # Desired thrust of the engine (lbf)
 p_c = 500 # Optimal chamber pressure (psi)
 OF = 1.2 # Nominal OF Ratio
-K = 0.35 # Desired injector stiffness (N/A)
-g = 9.81 # Gravitational constan (m/s^2)
+K_fu = 0.35 # Desired Fuel injector stiffness (N/A)
+K_ox = 1 # Desired Ox injector stiffness (N/A)
+g = 9.81 # Gravitational constant (m/s^2)
 ox_orifice_num = 12 # Number of oxidizer orifices
 fuel_orifice_num = 2 * ox_orifice_num # Number of fuel orifices
 
@@ -53,26 +54,29 @@ m_dot = F_N / (Isp * g) # Total combined mass flow (kg/s)
 m_dot_fu = m_dot / (1 + OF) # Fuel mass flow (kg/s)
 m_dot_ox = m_dot - m_dot_fu # Oxidizer mass flow (kg/s)
 
-dp = K * p_c # Desired pressure drop across injector (psi)
+dp_fu = K_fu * p_c # Desired pressure drop across injector (psi)
+dp_ox = K_ox * p_c
 
-p_m = p_c + dp # Manifold pressure (psi)
+p_m_fu = p_c + dp_fu # Manifold pressure (psi)
+p_m_ox = p_c + dp_ox # Ox manifold pressure (psi)
 
-p_m_pa = p_m * 6894.76 # Manifold pressure (Pa)
-dp_pa = dp * 6894.76 # Pressure drop (Pa)
+p_m_ox_pa = p_m_ox * 6894.76 # Manifold pressure (Pa)
 
-oxygen = Fluid(FluidsList.Oxygen).with_state(Input.pressure(p_m_pa), Input.temperature(20))
+dp_fu_pa = dp_fu * 6894.76 # Pressure drop (Pa)
+
+oxygen = Fluid(FluidsList.Oxygen).with_state(Input.pressure(p_m_ox_pa), Input.temperature(20))
 rho_ox = oxygen.density # Oxygen density at manifold pressure and ambient temperature (kg/m^3)
 
 rho_fu = 810 # RP-1 density at ambient temp and pressure (may want to update this to match manifold press), (kg/m^3)
 
 # Compressible flow equation
-A_fu = m_dot_fu / (C_d_fu * np.sqrt(2 * rho_fu * dp_pa)) # Total fuel injection area (m^2)
+A_fu = m_dot_fu / (C_d_fu * np.sqrt(2 * rho_fu * dp_fu_pa)) # Total fuel injection area (m^2)
 d_fu = 2 * np.sqrt((A_fu / fuel_orifice_num) / np.pi) # Fuel orifice diameter (m)
 d_fu_in = d_fu * 39.3701 # Fuel orifice diameter (in)
 
 # Incompressible flow equation (Choked condition)
-A_ox = m_dot_ox / (C_d_ox * np.sqrt(2 * rho_ox * p_m_pa * (gamma / (gamma - 1))
-* ((p_c_pa / p_m_pa) ** (2 / gamma) - (p_c_pa / p_m_pa) ** ((gamma + 1) / gamma)))) # Total ox injection area (m^2)
+A_ox = m_dot_ox / (C_d_ox * np.sqrt(2 * rho_ox * p_m_ox_pa * (gamma / (gamma - 1))
+* ((p_c_pa / p_m_ox_pa) ** (2 / gamma) - (p_c_pa / p_m_ox_pa) ** ((gamma + 1) / gamma)))) # Total ox injection area (m^2)
 d_ox = 2 * np.sqrt((A_ox / ox_orifice_num) / np.pi) # Ox orifice diameter (m)
 d_ox_in = d_ox * 39.3701 # Ox orifice diameter (in)
 
@@ -87,6 +91,8 @@ print(f"Ox mass flow: {m_dot_ox:.3f} kg/s")
 
 print(f"\nTotal fuel injection area: {A_fu:.6f} m^2")
 print(f"Fuel orifice diameter: {d_fu_in:.3f} in")
+print(f"Fuel feed pressure: {p_m_fu:.2f} psi")
 print(f"\nTotal oxidizer injection area: {A_ox:.6f} m^2")
 print(f"Oxidizer orifice diameter: {d_ox_in:.3f} in")
+print(f"Oxidizer feed pressure: {p_m_ox:.2f} psi")
 print("")
