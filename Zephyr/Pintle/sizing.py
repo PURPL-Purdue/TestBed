7c427e9f066f6c4ac4182626b45bc34415d199c6
OptionalNose <optionalnose@outlook.com>
2026-04-01 13:29:43 -0400

    Add Magnet Code
    Hopefully it's correct

--- Zephyr/Pintle/sizing.py
+++ Zephyr/Pintle/sizing.py
@@ -1,9 +1,11 @@
-from math import pi, sqrt, isnan, atan2, cos, sin
+from math import pi, sqrt, isnan, atan2, cos, sin, acos, asin, ceil
 from rocketcea.cea_obj import CEA_Obj
 import numpy as np
 import matplotlib.pyplot as plt
 
 import get_oring
+import cv_manifold
+import orb
 
 in_to_m = 0.0254
 lb_to_kg = 0.4536
@@ -21,28 +23,28 @@ def get_throttle_target(Pc, OF, ep):
     return actual
 
 
-stiffness = 0.10
+stiffness = 0.15
 sleeve_thickness = 0.100
 pintle_wall = 0.100
-blockage_factor = 0.7
+blockage_factor = 0.85
 
 Cdr = 0.61
 Cda = 0.98
 
 Pa = 14.695
 
-max_thrust = 1620
-Pc = 325
-of_ratio = 2
-Mdot = 6.28
+max_thrust = 1060
+Pc = 225
+of_ratio = 1.5
+Mdot = 4.36
 Mf = Mdot / (1 + of_ratio)
 Mo = Mf * of_ratio
-rhof = 768.09 * (in_to_m ** 3) / lb_to_kg
+rhof = 785 * (in_to_m ** 3) / lb_to_kg
 rhoo = 1205 / lb_to_kg * (in_to_m ** 3)
 
-expansion_ratio = 4.101
+expansion_ratio = 3.274
 chamber_diameter = (0.107) / in_to_m
-exit_area = expansion_ratio * ((0.05337 / 2) / in_to_m)**2 * pi
+exit_area = expansion_ratio * ((0.05309 / 2) / in_to_m)**2 * pi
 
 Dp = 0.2 * chamber_diameter
 Dbore = Dp - 2 * pintle_wall
@@ -66,6 +68,9 @@ Pdf = (Mf ** 2 / (2 * Cda ** 2 * Aann ** 2 * rhof)) / lbf_to_lbins
 ox_feed = (1 + stiffness) * Pc
 fuel_feed = Pdf + Pc
 
+min_rp1_diam = 2 * sqrt(Aann / pi)
+min_lox_diam = 2 * sqrt(Aslot / pi)
+
 print(f"Pintle Tip Outer Diameter (in): {Dp:.3f}")
 print(f"Pintle Tip Inner Diameter (in): {Dbore:.3f}")
 print(f"Pintle Slot Height (in): {slot_height:.3f}")
@@ -75,35 +80,10 @@ print(f"Annulur Gap (in): {(Dann - Dp - 2 * sleeve_thickness)/2:.3f}")
 print(f"Fuel Stiffness (%): {100 * Pdf/Pc:.1f}")
 print(f"LOx Feed Pressure (psia): {ox_feed: .0f}")
 print(f"RP1 Feed Pressure (psia): {fuel_feed: .0f}")
-print(f"Mininum LOx Inlet Diameter (in): {2*sqrt(Aslot / pi):.3f}")
-print(f"Mininum RP1 Inlet Diameter (in): {2*sqrt(Aann / pi):.3f}")
+print(f"Mininum LOx Inlet Diameter (in): {min_lox_diam:.3f}")
+print(f"Mininum RP1 Inlet Diameter (in): {min_rp1_diam:.3f}")
 
-
-thread_diam = None
-thread_tpi = None
-thread_name = None
-with open('unf_threads.csv') as thread_csv:
-    lines = thread_csv.readlines()
-    for line in lines:
-        data = line.split(",")
-        diam = float(data[0])
-        tpi = int(data[1])
-        name = data[2].strip()
-        if diam >= Dp + 0.100:
-            thread_diam = diam
-            thread_tpi = tpi
-            thread_name = name
-            break
-
-if thread_diam is None or thread_tpi is None or thread_name is None:
-    print(f"[ERROR]: Pintle Diamter {Dp:0.3f}in is larger than the largest thread size!")
-    exit()
-
-print(f"\n")
-print(f"Pintle Thread: {thread_name} (OD: {thread_diam:.3f}; TPI: {thread_tpi})")
-
-
-cea = CEA_Obj(oxName='LOX', fuelName='RP1')
+cea = CEA_Obj(oxName='LOX', fuelName='Ethanol')
 
 throttles = np.linspace(1.0, 0.01, 100)
 chamber_pressures = np.empty_like(throttles)
@@ -135,8 +115,6 @@ for i in range(len(throttles)):
     sonic = cea.get_SonicVelocities(chamber_pressures[i], Mo / Mf, expansion_ratio)[2] * 12
 
     mass_flows[i] = rhoe * mach * sonic * exit_area
-
-print(f"{mass_flows}")
 
 ox_fraction = Mo / (Mo + Mf)
 fuel_fraction = Mf / (Mo + Mf)
@@ -173,7 +151,7 @@ for i in range(len(throttles)):
 
 slotheights = np.empty_like(throttles)
 annulargaps = np.empty_like(throttles)
-alignment_limit = 0.0005 / 0.15
+alignment_limit = 0.004
 min_slotheight = None
 min_throttle = None
 for i in range(len(throttles)):
@@ -187,183 +165,29 @@ sleeve_max = (Dann - Dp) / 2 - np.min(annulargaps)
 print(f"Sleeve Thickness at 0% Throttle (in): {sleeve_max:.3f}")
 print(f"Reliable Minimum Throttle (%): {min_throttle * 100:.0f}")
 
-sleeve_force = fuel_feed * pi * ((Dp + sleeve_max * 2)**2 - (Dp + sleeve_thickness * 2)**2) / 4.0
-print(f"Force on Sleeve (lbf): {sleeve_force:.1f}")
-thread_angle = atan2(1/thread_tpi, pi * thread_diam)
-servo_torque = (thread_diam / 2) * sleeve_force * (friction * cos(thread_angle) + sin(thread_angle)) / (cos(thread_angle) - friction * sin(thread_angle))
-
-print(f"Torque on Servo (lbf*in): {servo_torque:.1f}")
-print(f"Actuation Precision (in): ±{thread_tpi/servo_encoder_count:.3f}")
-
 get_oring.load_orings()
 
-key_height = 0.100
-thread_length = 3*gland['spacing']+2*gland['width']+2*slot_height+key_height
-
-with open("pintle_shaft.exp", "w") as expr:
-    expr.write(f"""
-    // Version:  3
-    [Inch]OR={Dp/2:.4f}
-    [Inch]drill_tip=0.25
-    [Inch]flange_rad=1.5
-    [Inch]flange_thickness=0.25
-    [Degrees]p0=0
-    [Degrees]p1=360
-    [Inch]p6=wall
-    [Inch]p13=wall*sqrt(2)
-    [Inch]p15=slot_circum/slot_count
-    [Inch]p21=slot_height
-    [Inch]p22=wall
-    [Inch]p23=2.5*wall
-    p31=slot_count
-    [Degrees]p32=15
-    [Inch]p33=1
-    [Degrees]p34=360
-    p35=1
-    [Inch]p36=1
-    [Inch]p37=0
-    [Degrees]p38=0
-    [Inch]p39=2*OR+3*slot_height
-    [Inch]slot_circum={slot_circum:.3f}
-    slot_count=24
-    [Inch]slot_height={slot_height:.3f}
-    [Inch]thread_rad={thread_diam/2:.4f}
-    [Inch]upper_flat=2*slot_height+1.5
-    [Inch]wall={pintle_wall:.3f}
-    [Inch]thread_length={thread_length:.3f}
-    """)
-
-with open("sleeve.exp", "w") as expr:
-    gland = get_oring.get_gland_size()
-
-    expr.write(f"""
-    // Version:  3
-    [Inch]p0={sleeve_thickness:.3f}
-    [Inch]p5={Dp/2:.2f}
-    [Inch]p47={slot_height+Dp:.3f}
-    [Inch]p48={slot_height:.3f}
-    [Inch]p45={sleeve_max-sleeve_thickness:.3f}
-    [Inch]p117={(thread_diam-Dp)/2-5*sqrt(3)/8/thread_tpi/2:.3f}
-    [Inch]p134={thread_length:.3f}
-    [Degrees]p7=360
-    [Degrees]p102=45
-    [Inch]p103={Dp/2+sleeve_max:.3f}
-    [Inch]p50=sleeve_key_height
-    [Inch]p116=middle_plate_height-lox_manifold_height-sleeve_key_height-gland['width']
-    [Inch]sleeve_key_height=0.100
-    [Inch]sleeve_key_thickness=0.100
-    [Inch]p14=key_length
-    [Inch]key_length=0.2
-    [Inch]lox_manifold_height=0.279
-    """)
-
-shelf_length=1.4
-lox_cv_length=2.125
-side_thickness=0.5
-
-with open("faceplate.exp", "w") as expr:
-    expr.write(f"""
-    // Version: 3
-    [Inch]annulus_rad={Dann/2:.4f}
-    [Inch]chamber_rad={chamber_diameter/2:.4f}
-    [Inch]curvature_rad={chamber_diameter*7/9/2:.4f}
-    [Degrees]slant_angle={2*180/pi*atan2(sleeve_max-sleeve_thickness,slot_height):.4f}
-    [Inch]slant_length=0.5
-    [Inch]side_thickness={side_thickness:.3f}
-    [Inch]flange_rad=1.201
-    [Inch]p1=annulus_rad
-    [Degrees]p2=slant_angle
-    [Inch]p3=slant_length
-    [Inch]p4=chamber_rad
-    [Inch]p5=curvature_rad
-    [Inch]p6=1.5*annulus_rad
-    [Inch]p10=side_thickness
-    [Degrees]p7=0
-    [Degrees]p8=360
-    [Inch]p21=chamber_rad+1.5*annulus_rad
-    [Inch]p22=flange_rad
-    [Inch]p43=-side_thickness
-    [Inch]p47=0.5
-    [Inch]p57=chamber_rad+1.5*annulus_rad+flange_rad/2
-    p175="5/8-11"
-    p176="0.75"
-    p177="Inch UNC"
-    """)
-
-
-with open("middleplate.exp", "w") as expr:
-    gland = get_oring.get_gland_size()
-
-    radial_rings = get_oring.min_inner(sleeve_max*2 + Dp)
-
-    expr.write(f"""
-    // Version: 3
-    [Inch]oring_backing={gland['spacing']:.3f}
-    [Inch]oring_width={gland['width']:.3f}
-    [Inch]oring_depth={gland['depth']:.3f}
-    [Inch]slot_height={slot_height:.3f}
-    [Inch]key_height=0.100
-    [Inch]flange_rad=1.201
-    [Inch]middle_plate_thickness={2*slot_height+2*gland['width']+3*gland['spacing']+Dp:.3f}
-    [Inch]curvature_rad={chamber_diameter*7/9/2:.4f}
-    [Inch]slant_distance={radial_rings['ID']/2:.4f}
-    [Inch]side_thickness={side_thickness:.3f}
-    [Inch]shelf_length={shelf_length:.4f}
-    [Inch]p5=slant_distance
-    [Inch]p6=curvature_rad
-    [Inch]p7={chamber_diameter/2:.4f}
-    [Inch]p9=middle_plate_thickness
-    [Inch]lox_cv_length=2.125
-    [Inch]p13=shelf_length
-    [Inch]lox_cv_height=0.25
-    [Inch]p8={Dann/2*1.5:.4f}
-    [Inch]p10=side_thickness
-    [Inch]lox_cv_length={lox_cv_length:.3f}
-    [Degrees]p14=0
-    [Degrees]p15=360
-    [Inch]p17=lox_cv_length+slant_distance+shelf_length/2
-    p175="1/2-13"
-    p176="0.75"
-    p177="Inch UNC"
-    p225={chamber_diameter/2+1.5*Dann/2:.4f}
-    p268=flange_rad
-    p267=p225+(flange_rad/2)
-    p266=0.5
-    p435="5/8-11"
-    p436="0.75"
-    p437="Inch UNC"
-    """)
-
-with open("topplate.exp", "w") as expr:
-    expr.write(f"""
-    [Inch]top_plate_thickness=0.75
-    [Inch]shelf_length={shelf_length:.4f}
-    [Inch]slant_distance={Dp:.3f}
-    [Inch]p5=slant_distance
-    [Inch]p7=2.125+shelf_length
-    [Inch]p20=lox_cv_length+slant_distance+shelf_length/2
-    p69="Inch UNC"
-    p71="1/2-13"
-    p73="0.75"
-    """)
-
-
+radial_rings = get_oring.radial_min_rod_od(sleeve_max*2 + Dp)
+radial_gland = get_oring.get_radial_gland_size(radial_rings['num'])
+print(f"Middle Plate Radial Rings: 2x #{radial_rings['num']:03d}")
 
 fig1, ax1 = plt.subplots(2, 3)
 fig2, ax2 = plt.subplots(2, 2)
 ax1[0,0].plot(throttles, chamber_pressures)
 ax1[0,0].set_xlim(0.0, 1.0)
 ax1[0,0].set_ylim(0.0, Pc)
-ax1[0,0].set_xlabel("Throttle Level (ratio)")
+ax1[0,0].set_xlabel("Throttle Level (actual/full)")
 ax1[0,0].set_ylabel("Chamber Pressure (psia)")
 ax1[0,0].set_title("Chamber Pressure vs. Throttle")
+ax1[0,0].grid()
 
 ax1[1,0].plot(throttles, mass_flows)
 ax1[1,0].set_xlim(0.0, 1.0)
 ax1[1,0].set_ylim(0.0, Mo + Mf)
-ax1[1,0].set_xlabel("Throttle Level (ratio)")
+ax1[1,0].set_xlabel("Throttle Level (actual/full)")
 ax1[1,0].set_ylabel("Total Mass Flow (lbm / s)")
 ax1[1,0].set_title("Total Mass Flow vs. Throttle")
+ax1[1,0].grid()
 
 ax1[0,1].plot(chamber_pressures, mass_flows)
 ax1[0,1].set_xlim(np.min(chamber_pressures), Pc)
@@ -371,60 +195,68 @@ ax1[0,1].set_ylim(0.0, Mo + Mf)
 ax1[0,1].set_xlabel("Chamber Pressure (psia)")
 ax1[0,1].set_ylabel("Total Mass Flow (lbm / s)")
 ax1[0,1].set_title("Total Mass Flow vs. Chamber Pressure")
+ax1[0,1].grid()
 
 ax2[0,0].plot(throttles, lox_areas)
 ax2[0,0].plot(throttles, fuel_areas)
 ax2[0,0].set_xlim(0.0, 1.0)
 ax2[0,0].set_ylim(0.0, max(Aann, Aslot))
-ax2[0,0].set_xlabel("Throttle Level (ratio)")
+ax2[0,0].set_xlabel("Throttle Level (actual/full)")
 ax2[0,0].set_ylabel("Orifice Areas (sq.in)")
 ax2[0,0].set_title("Orifice Areas vs. Throttle Level")
-ax2[0,0].legend(["LOx", "RP1"])
+ax2[0,0].legend(["LOx", "Ethanol"])
+ax2[0,0].grid()
 
 ax1[1,1].plot(throttles, tmrs)
 ax1[1,1].plot(throttles, lmrs)
 ax1[1,1].set_xlim(0.0, 1.0)
 ax1[1,1].set_ylim(0.0, 1.05 * max(np.max(tmrs), np.max(lmrs)))
-ax1[1,1].set_xlabel("Throttle Level (ratio)")
-ax1[1,1].set_ylabel("TMR and LMR (ratio)")
+ax1[1,1].set_xlabel("Throttle Level (actual/full)")
+ax1[1,1].set_ylabel("TMR and LMR (radial/annular momentum)")
 ax1[1,1].set_title("TMR and LMR vs. Throttle Level")
 ax1[1,1].legend(["TMR", "LMR"])
+ax1[1,1].grid()
 
-ax2[0,1].plot(throttles, lox_stiffnesses)
-ax2[0,1].plot(throttles, fuel_stiffnesses)
+ax2[0,1].plot(throttles, 100.0 * lox_stiffnesses)
+ax2[0,1].plot(throttles, 100.0 * fuel_stiffnesses)
 ax2[0,1].set_xlim(0.0, 1.0)
-ax2[0,1].set_ylim(0.0, max(np.max(lox_stiffnesses), np.max(fuel_stiffnesses)))
-ax2[0,1].set_xlabel("Throttle Level (ratio)")
-ax2[0,1].set_ylabel("Stiffness (ratio)")
+ax2[0,1].set_ylim(0.0, 100.0 * max(np.max(lox_stiffnesses), np.max(fuel_stiffnesses)))
+ax2[0,1].set_xlabel("Throttle Level (actual/full)")
+ax2[0,1].set_ylabel("Stiffness (%)")
 ax2[0,1].set_title("Stiffness vs. Throttle Level")
-ax2[0,1].legend(["LOx", "RP1"])
+ax2[0,1].legend(["LOx", "Ethanol"])
+ax2[0,1].grid()
+
 
 ax1[0,2].plot(throttles, spray_angles)
 ax1[0,2].set_xlim(0.0, 1.0)
 ax1[0,2].set_ylim(np.min(spray_angles), np.max(spray_angles))
-ax1[0,2].set_xlabel("Throttle Level (ratio)")
+ax1[0,2].set_xlabel("Throttle Level (actual/full)")
 ax1[0,2].set_ylabel("Spray Angle (degrees)")
 ax1[0,2].set_title("Spray Angle vs. Throttle Level")
+ax1[0,2].grid()
 
 ax1[1,2].plot(throttles, ispvacs)
 ax1[1,2].plot(throttles, ispambs)
 ax1[1,2].vlines(separation_point, 0.0, 1.05 * np.max(ispvacs), linestyles='dashed', label='Flow Separation')
 ax1[1,2].set_xlim(0.0, 1.0)
 ax1[1,2].set_ylim(0.0, 1.05 * np.max(ispvacs))
-ax1[1,2].set_xlabel("Thrust Level (ratio)")
+ax1[1,2].set_xlabel("Thrust Level (actual/full)")
 ax1[1,2].set_ylabel("ISP (s)")
 ax1[1,2].set_title("Vacuum and Sea Level ISP vs. Throttle Level")
 ax1[1,2].legend(["Vacuum", "Sea Level"])
+ax1[1,2].grid()
 
 ax2[1,0].plot(throttles, slotheights)
 ax2[1,0].plot(throttles, annulargaps)
 ax2[1,0].set_xlim(0.0, 1.0)
 ax2[1,0].set_ylim(0.0, max(np.max(slotheights), np.max(annulargaps)))
 ax2[1,0].vlines(min_throttle, 0.0, max(np.max(slotheights), np.max(annulargaps)), linestyles='dashed', label='Alignment Limit')
-ax2[1,0].set_xlabel("Thrust Level (ratio)")
+ax2[1,0].set_xlabel("Thrust Level (actual/full)")
 ax2[1,0].set_ylabel("Size (in)")
 ax2[1,0].set_title("Slot Height and Annular Gap vs. Throttle Level")
-ax2[1,0].legend(["Slot Height", "Annular Gap"])
+ax2[1,0].legend(["Slot Height", "Annular Gap", "Reliable Minimum Throttle"])
+ax2[1,0].grid()
 
 ax2[1,1].plot(slotheights, annulargaps)
 ax2[1,1].set_xlim(np.min(slotheights), np.max(slotheights))
@@ -433,5 +265,7 @@ ax2[1,1].vlines(min_slotheight, 0.0, np.max(annulargaps), linestyles='dashed', l
 ax2[1,1].set_xlabel("Slot Height (in)")
 ax2[1,1].set_ylabel("Annular Gap (in)")
 ax2[1,1].set_title("Annular Gap vs. Slot Height")
+ax2[1,1].legend(["Annular Gap", "Reliable Minimum Throttle"])
+ax2[1,1].grid()
 
 plt.show()
